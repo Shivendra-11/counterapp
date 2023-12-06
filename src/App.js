5b7657d042c7f457d1a0c873b4d7768748b946bf
shivendra <shivendrakeshari11@gmail.com>
2023-12-06 15:51:48 +0530

    made changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
      <div>
       <h2  className='bg-black  text-white ' >Lorem ipsum dolor sit.</h2>
+      <h3  className='bg-red-700  text-center   text-xl  '  >this is the second heading</h3>
      </div>
   );
 }
